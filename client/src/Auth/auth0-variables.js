f314304fab0648e42eb398b564a47abd10d6280a
Russell Harben <russellharben@gmail.com>
2019-04-13 12:25:01 -0400

    new domain in Auth_Config

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
-  domain: 'roundup.auth0.com',
+  domain: 'pennywise.auth0.com',
   clientId: 'gE70G-uomrWUbB8Dis5Vw7T9jspPPjZo',
   callbackUrl: 'http://localhost:3000/callback'
   // apiUrl: 'http://roundup.auth0.com/callback'
