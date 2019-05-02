e06fa515dd50ef7d289b1f0445a95d7eeb1abfc2
KHAUNA <jtwinturbo@gmail.com>
2019-05-02 17:34:28 -0400

    tin1

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ var bodyParser = require('body-parser');
 var moment = require('moment');
 var plaid = require('plaid');
 
-var APP_PORT = envvar.number('APP_PORT', 8034);
+var APP_PORT = process.env.PORT || envvar.number('APP_PORT', 8034);
 var PLAID_CLIENT_ID = envvar.string('PLAID_CLIENT_ID', process.env.PLAID_CLIENT_ID);
 var PLAID_SECRET = envvar.string('PLAID_SECRET', process.env.PLAID_SECRET);
 var PLAID_PUBLIC_KEY = envvar.string('PLAID_PUBLIC_KEY', process.env.PLAID_PUBLIC_KEY);
